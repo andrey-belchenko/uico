4acb17cd0c2dd2d3b717a6adfbbe60a243ea833c
Andrey Belchenko <abelchenko@conteq.ru>
2024-07-13 02:04:14 +0300

    1

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.printMsg = printMsg;
+exports.info = info;
+function printMsg() {
+    console.log("This is a message from the demo package");
+}
+function info() {
+    return "This is a demo package";
+}
---
+++ index.ts
@@ -0,0 +1,8 @@
+export function printMsg() {
+    console.log("This is a message from the demo package");
+}
+
+
+export function info() {
+     return "This is a demo package";
+}
